730b7e9e2f10c41e5db3f31293308fb74d503492
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-05-24 16:28:18 +0800

    Add contact static page
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -36,4 +36,16 @@ describe "Static pages" do
       page.should have_selector('title', :text => "Twitterish 2 | About") 
     end
   end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "Twitterish 2 | Contact Us") 
+    end
+  end
 end
