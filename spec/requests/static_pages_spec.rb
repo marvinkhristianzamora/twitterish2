25a7b906312133cf9da4c4f2d9e3e596f0292a4a
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-05-24 12:17:02 +0800

    Cleanup static pages (included layout)
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,22 +4,36 @@ describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Twitterish 2'" do
       visit '/static_pages/home'
-      page.should have_content('Twitterish 2')
+      page.should have_selector('h1', :text => 'Twitterish 2')
     end
-
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Twitterish 2 | Home") 
+    end
   end
  
   describe "Help page" do
     it "should have thet content 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Twitterish 2 | Help") 
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Twitterish 2 | About") 
     end
   end
 end
