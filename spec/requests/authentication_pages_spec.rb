b403282ab78f2f26781850fd842ee4f3c1041aa2
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-06 14:23:03 +0800

    Add implementation of user authorization in edit pages
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,7 +49,7 @@ describe "Authentication" do
         describe "visiting the edit page" do
           before { visit edit_user_path(user) }
           it { should have_link('Sign in', href: signin_path) }
-          it { should_not have_title('Edit user') }
+          it { should_not have_title(full_title('Edit user')) }
         end
 
         describe "submitting to the update_action" do
@@ -57,7 +57,23 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+    end
 
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@email.com") }
+
+      before { valid_signin(user) }
+      
+      describe "editing a different user" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_title(full_title('Edit user')) }
+      end
+
+      describe "submitting a PATCH request to the User#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
     end
   end
 end
