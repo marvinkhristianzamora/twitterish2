7666ab86859b3635d40d30ff2582516a26659c51
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-07 16:16:34 +0800

    Initiate micropost model
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- db/structure.sql
+++ db/structure.sql
@@ -1,5 +1,7 @@
+CREATE TABLE "microposts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" varchar(255), "user_id" integer, "created_at" datetime, "updated_at" datetime);
 CREATE TABLE "schema_migrations" ("version" varchar(255) NOT NULL);
 CREATE TABLE "users" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "name" varchar(255), "email" varchar(255), "created_at" datetime, "updated_at" datetime, "password_digest" varchar(255), "remember_token" varchar(255), "admin" boolean DEFAULT 'f');
+CREATE INDEX "index_microposts_on_user_id_and_created_at" ON "microposts" ("user_id", "created_at");
 CREATE UNIQUE INDEX "index_users_on_email" ON "users" ("email");
 CREATE INDEX "index_users_on_remember_token" ON "users" ("remember_token");
 CREATE UNIQUE INDEX "unique_schema_migrations" ON "schema_migrations" ("version");
@@ -12,3 +14,5 @@ INSERT INTO schema_migrations (version) VALUES ('20130531060443');
 INSERT INTO schema_migrations (version) VALUES ('20130605044649');
 
 INSERT INTO schema_migrations (version) VALUES ('20130606081819');
+
+INSERT INTO schema_migrations (version) VALUES ('20130607074053');
---
+++ spec/models/micropost_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Micropost do
+  
+  let(:user) { FactoryGirl.create(:user) }
+  before do
+    @micropost = Micropost.new(content: "Test Micropost", user_id: user.id)
+  end
+
+  subject { @micropost }
+
+  it { should respond_to(:content) }
+  it { should respond_to(:user_id) }
+
+  it { should be_valid }
+
+  describe "when user id is nil" do
+    before { @micropost.user_id = nil }
+    it { should_not be_valid }
+  end
+end
